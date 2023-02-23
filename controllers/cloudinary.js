9db61d290a2b971d4e2dd3b7b0eeda780563c8db
sttwood <settawut.borrisut@gmail.com>
2023-02-23 18:56:52 +0700

    rollback

--- controllers/cloudinary.js
+++ controllers/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary')
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: 'dpl9wgyem',
+    api_key: '437185226335457',
+    api_secret: '0TwofY5gR7OiHs3P91J5ImVvtk4'
 })
 
 // POST
